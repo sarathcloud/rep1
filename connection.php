6a8c7e4c49f121e59c344934bcebdc05c21bcfa4
Mohammed Shafi <shafisnpni@gmail.com>
2021-07-26 12:23:38 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$hostname="cobsha.cbeinepcnbhk.us-east-2.rds.amazonaws.com";
+$user_name="admin";
+$password="Shafi2015#";
+$db="dbfood";
+$con=mysqli_connect($hostname,$user_name,$password,$db);
+
+ 
+
+?>
